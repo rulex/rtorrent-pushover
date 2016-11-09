44a0d6441f809d17122c63f986b07dd945fd0b6b
Lars Wikberg <lars.wikberg@gmail.com>
2016-11-09 14:50:06 +0200

    added notify-send and tmux notifications

--- rtorrent-pushover.sh
+++ rtorrent-pushover.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+# pushover settings
 userkey="XXXXXXXX"
 apitoken="ZZZZZZZZZZZ"
 
 tname=$1
 ts=$(date +%s)
 
-curly=`curl https://api.pushover.net/1/messages.json -d "token=${apitoken}&user=${userkey}&title=Torrent&message=${tname}&timestamp=${ts}"`
-echo ${ts} ${tname} ${curly} >> ~/.rtorrent-pushover.log
+# notify-send
+if [[ -x $( which notify-send ) ]]; then
+    notify-send -t 60000 -a rtorrent "${tname}"
+fi
 
+# also notify tmux
+if [[ -x $( which tmux ) ]]; then
+    tmux display-message "Torrent: ${tname}"
+fi
+
+# pushover
+curly=$(curl https://api.pushover.net/1/messages.json -d "token=${apitoken}&user=${userkey}&title=Torrent&message=${tname}&timestamp=${ts}")
+echo ${ts} ${tname} ${curly} >> ~/.rtorrent-pushover.log
